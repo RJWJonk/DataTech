f8c5e4873997b6b5e5f81cc58b76ec6832c6674d
Tim Beishuizen <TimBeishuizen@hotmail.com>
2015-05-19 20:38:08 +0200

    Working query in ARFfilter

--- ARFDataTech/src/arfdatatech/ARFFilter.java
+++ ARFDataTech/src/arfdatatech/ARFFilter.java
@@ -5,8 +5,8 @@
  */
 package arfdatatech;
 import java.util.BitSet;
-import java.lang.*;
 import java.util.Queue;
+import java.util.LinkedList;
 
 /**
  *
@@ -18,6 +18,7 @@ public class ARFFilter extends Filter {
     static int numElements = 0;
     private final int maxElements;
     private BitSet ARFTree;
+    private int treeSize;
     private BitSet leafValues;
     private int[] filterRange;
     
@@ -26,72 +27,88 @@ public class ARFFilter extends Filter {
         super(name);
         this.maxElements = maxElements;
         int[] range = {0, 1};
-        setTree(new BitSet(2), new BitSet(2), range);
+        setTree(new BitSet(2), new BitSet(2), 2, range);
     }
 
-    public void setTree(BitSet tree, BitSet values, int[] range) {
+    public void setTree(BitSet tree, BitSet values, int size, int[] range) {
         ARFTree = tree;
         leafValues = values;
+        treeSize = size;
         this.filterRange = range;
     }
     
     @Override
     public boolean query(int key_min, int key_max) {
-        int[][] curRange = new int[1][2];
-        curRange[1] = filterRange;
+        Queue<int[]> rangeList;
+        rangeList = new LinkedList();
+        rangeList.add(filterRange.clone());
         int indRange = 1;
         int curTree = 1;
         int curLeaf = 1;
         
         /* All of the tree is searched for the range */
-        while (curTree < ARFTree.size()) {
-
+        while (curTree < treeSize) {
+            
+            int[] curRange = rangeList.poll();
+            System.out.println(curRange[0] + "" + curRange[1]);
+            
+            int midRange = (curRange[0] + curRange[1]) / 2;
             /* For a node with two leaves as children */
-            if (ARFTree.get(curTree) == false && ARFTree.get(2 * curTree + 1) == false) {
-                int midRange = (curRange[1][1] + curRange[1][2]) / 2;
+            if (ARFTree.get(curTree) == false && ARFTree.get(curTree + 1) == false) {
+                
+                System.out.println(curRange[0] + "" + midRange);
+
                 /* Checks left child if in range and if true */
-                if (checkRange(curRange[1][1], midRange, key_min, key_max) && leafValues.get(curLeaf) == true) {
+                if (checkRange(curRange[0], midRange, key_min, key_max) && leafValues.get(curLeaf) == true) {
                     return true;
                 }
-                ++curLeaf;
+                
+                System.out.println(midRange + "" + curRange[1]);
+                
+                ++ curLeaf;
                 /* Checks right child if in range and if true */
-                if (checkRange(midRange, curRange[1][2], key_min, key_max) && leafValues.get(curLeaf) == true) {
+                if (checkRange(midRange + 1, curRange[1], key_min, key_max) && leafValues.get(curLeaf) == true) {
                     return true;
                 }
                 ++curLeaf;
+                
 
                 /* For a node with the only a leaf in the right child */
-            } else if (ARFTree.get(curTree) == true && ARFTree.get(2 * curTree + 1) == false) {
-                int midRange = (curRange[1][1] + curRange[1][2]) / 2;
+            } else if (ARFTree.get(curTree) == true && ARFTree.get(curTree + 1) == false) {
                 /* Checks right child if in range and if true */
-                if (checkRange(midRange, curRange[1][2], key_min, key_max) && leafValues.get(curLeaf) == true) {
+                if (checkRange(midRange + 1, curRange[1], key_min, key_max) && leafValues.get(curLeaf) == true) {
                     return true;
                 }
                 ++curLeaf;
-                curRange[1][2] = midRange;
+                
+                curRange[1] = midRange;
+                rangeList.add(curRange);
 
                 /* For a node with the only a leaf in the left child */
-            } else if (ARFTree.get(curTree) == false && ARFTree.get(2 * curTree + 1) == true) {
-                int midRange = (curRange[1][1] + curRange[1][2]) / 2;
+            } else if (ARFTree.get(curTree) == false && ARFTree.get(curTree + 1) == true) {
+
                 /* Checks left child if in range and if true */
-                if (checkRange(curRange[1][1], midRange, key_min, key_max) && leafValues.get(curLeaf) == true) {
+                if (checkRange(curRange[0], midRange, key_min, key_max) && leafValues.get(curLeaf) == true) {
                     return true;
                 }
                 ++curLeaf;
+                
                 /* Set new range as current range */
-                curRange[1][1] = midRange;
+                curRange[0] = midRange + 1;
+                rangeList.add(curRange);
             
                 /* For a node with no child leaves */
-            } else if (ARFTree.get(curTree) == true && ARFTree.get(2 * curTree + 1) == true) {
-                int midRange = (curRange[1][1] + curRange[1][2]) / 2;
+            } else if (ARFTree.get(curTree) == true && ARFTree.get(curTree + 1) == true) {
                 
                 /* Two ranges are made */
-                curRange[2][2] = curRange[1][2];
-                curRange[1][2] = midRange;
-                curRange[2][1] = midRange;
-
+                int[] botRange = {curRange[0], midRange};
+                rangeList.add(botRange);
+                
+                int[] topRange = {midRange + 1, curRange[1]};
+                rangeList.add(topRange);
             }
 
+            /* Place in the tree is updated */
             curTree = curTree + 2;
         }
 
@@ -99,7 +116,7 @@ public class ARFFilter extends Filter {
     }
     
     /* Checks whether the key is in the range */
-    private boolean checkRange(int range_min, int range_max, int key_min, int key_max) {
+    public boolean checkRange(int range_min, int range_max, int key_min, int key_max) {
         if ((range_min <= key_max) && (range_max >= key_min)) {
             return true;
         } else {
